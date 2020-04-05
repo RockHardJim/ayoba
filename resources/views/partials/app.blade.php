0437307dd2d8d1dced611ed9e63525c335fe7686
Tumelo Baloyi <tumelobaloyi360@gmail.com>
2020-04-05 19:48:25 +0200

    asset files for views

--- resources/views/partials/app.blade.php
+++ resources/views/partials/app.blade.php
@@ -1,1 +1,38 @@
-<?php
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ env('APP_NAME') }}</title>
+    <link rel="stylesheet" href="{{asset('css/materialize.css')}}">
+    <link rel="stylesheet" href="{{asset('css/loader.css')}}">
+    <link rel="stylesheet" href="{{asset('css/fontawesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+</head>
+<body>
+
+<!-- preloader -->
+<div class="loader-wrapper">
+    <div class="loader-cube"></div>
+</div>
+@yield('content')
+<footer class="home2">
+    <div class="container">
+        <div class="wrap-logo">
+            <h3>{{ env('APP_NAME') }}</h3>
+        </div>
+        <div class="footer-text">
+            <p>Copyright © All Right Reserved</p>
+        </div>
+    </div>
+</footer>
+
+<script src="{{asset('js/jquery.min.js')}}"></script>
+<script src="{{asset('js/materialize.min.js')}}"></script>
+<script src="{{asset('js/owl.carousel.min.js')}}"></script>
+<script src="{{asset('js/main.js')}}"></script>
+
+</body>
+</html>
