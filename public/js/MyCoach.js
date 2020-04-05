48e4ce1463b9e14cd6c2d3328194b2a6d3ffae97
Tumelo Baloyi <tumelobaloyi360@gmail.com>
2020-04-05 21:56:59 +0200

    asset files for views

---
+++ public/js/MyCoach.js
@@ -0,0 +1,19 @@
+$(function(){
+    "use strict";
+
+    var server = 'https://ayoba.tdigital.dev';
+    var loginurl = '/login';
+    var registerurl = '/register';
+
+    $(document).ready(function() {
+        isRegistered();
+    });
+
+    function isRegistered(){
+        if(window.location.href != server + loginurl || window.location.href != server + loginurl){
+
+        } else {
+
+        }
+    }
+});
